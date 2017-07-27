da5114956aac5666899a8417311141e365decd61
ENGINEERBOB <bobraphtoneafwata@gmail.com>
2017-07-27 18:16:18 +0300

    First Example Project

--- Install.sh
+++ Install.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
+
 sudo apt-get update
 sleep 1
 sudo apt-get install python-pip
 pip install pyserial
 git clone https://github.com/jsnyder/stm32loader.git
 cd stm32loader
+#Using the python tool to load the firmware device
+stm32loader.py -e -w -v FirstProject/FirstProject/Debug/bin/FirstProject.bin
