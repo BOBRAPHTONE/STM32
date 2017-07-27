095266801ecb96d0848e67beedf8538cb2114911
ENGINEERBOB <bobraphtoneafwata@gmail.com>
2017-07-27 18:17:32 +0300

    First Example Project

--- Install.sh
+++ Install.sh
@@ -8,3 +8,4 @@ git clone https://github.com/jsnyder/stm32loader.git
 cd stm32loader
 #Using the python tool to load the firmware device
 stm32loader.py -e -w -v FirstProject/FirstProject/Debug/bin/FirstProject.bin
+#Run the example found at https://stm32f4-discovery.net/2014/04/first-time-with-stm32f429-discovery/ using the IDE
